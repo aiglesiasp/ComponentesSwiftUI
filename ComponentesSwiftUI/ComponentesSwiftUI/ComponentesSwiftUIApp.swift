edbf34d244816554b12411b31c8bb9255ae6f4d0
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-11 18:19:25 +0100

    SplitViewIpad

--- ComponentesSwiftUI/ComponentesSwiftUI/ComponentesSwiftUIApp.swift
+++ ComponentesSwiftUI/ComponentesSwiftUI/ComponentesSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ComponentesSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            SlidesPanelView()
+            SplitViewIpad()
         }
     }
 }
---
+++ ComponentesSwiftUI/ComponentesSwiftUI/SplitViewIpad.swift
@@ -0,0 +1,34 @@
+//
+//  SplitViewIpad.swift
+//  ComponentesSwiftUI
+//
+//  Created by Aitor Iglesias Pubill on 11/12/22.
+//
+
+import SwiftUI
+
+struct SplitViewIpad: View {
+    @State private var heros = ["Goku", "Krillin", "Vegeta"]
+    @State private var selectedHero: String?
+    
+    
+    var body: some View {
+        
+        NavigationSplitView {
+            //Izquierda (lista)
+            List(heros, id: \.self, selection: $selectedHero) { cad in
+                Text("\(cad)")
+                
+            }
+        } detail: {
+            //Detalle (derecha)
+            Text(selectedHero ?? "Selecciona un heroe")
+        }
+    }
+}
+
+struct SplitViewIpad_Previews: PreviewProvider {
+    static var previews: some View {
+        SplitViewIpad()
+    }
+}
